c483733a02abe40feef0738dcd79105fe2738c42
Fredrik Larsen <fredrik.larsen@usit.uio.no>
2017-11-06 12:20:53 +0100

    Fix __version__ attribute

--- mkcrypt/__init__.py
+++ mkcrypt/__init__.py
@@ -12,7 +12,7 @@ from pkg_resources import get_distribution, DistributionNotFound
 
 
 try:
-    __version__ = get_distribution('mkcrypt').version
+    __version__ = get_distribution('passlib-crypt').version
 except DistributionNotFound:
     __version__ = None
 
