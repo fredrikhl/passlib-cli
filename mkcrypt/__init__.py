37b88f644e23366afc045d8e5d7a635a0d48e8f2
Fredrik Larsen <fredrik.h.larsen@gmail.com>
2023-05-10 17:10:21 +0200

    Add basic support for parameters
    This commit adds basic support for passlib method parameters.  It's all
    a bit messy, rewrite incoming, maybe?

--- mkcrypt/__init__.py
+++ mkcrypt/__init__.py
@@ -1,5 +1,5 @@
 # encoding: utf-8
-""" Make crypt strings using `passlib`. """
+""" A basic API that abstracts passlib.hash to my likings. """
 from __future__ import absolute_import, print_function
 
 import passlib
@@ -7,7 +7,7 @@ from collections import OrderedDict
 from distutils.version import LooseVersion
 from passlib.ifc import PasswordHash
 from passlib.registry import list_crypt_handlers, get_crypt_handler
-from passlib.utils.handlers import BackendMixin, HasUserContext, PrefixWrapper
+from passlib.utils.handlers import HasUserContext, PrefixWrapper
 from pkg_resources import get_distribution, DistributionNotFound
 
 
@@ -15,6 +15,17 @@ try:
     __version__ = get_distribution('passlib-crypt').version
 except DistributionNotFound:
     __version__ = None
+
+
+# TODO: Validate and transform params against <method>.setting_kwds?
+
+# 'salt'
+#   TODO: str?
+# 'truncate_error'
+#   TODO: How do we handle this?
+
+# TODO: Also, 'user' if require_user? Are there other params not in
+#       setting_kwds?
 
 
 # Use the 'hash' or the 'encrypt' method of passlib.ifc.PasswordHash?
@@ -48,7 +59,7 @@ def is_supported(method):
     :return bool:
         Return `True` if the method is supported.
     """
-    if issubclass(method, BackendMixin):
+    if hasattr(method, 'has_backend'):
         return method.has_backend()
     return hasattr(method, PASSLIB_HASH_NAME)
 
@@ -64,6 +75,8 @@ def requires_user(method):
     :return bool:
         Return `True` if the implementation requires a username parameter.
     """
+    if isinstance(method, PrefixWrapper):
+        return requires_user(method.wrapped)
     return issubclass(method, HasUserContext)
 
 
@@ -89,6 +102,14 @@ def get_class_name(method):
     return '{0.__module__}.{0.__name__}'.format(method)
 
 
+def get_settings(method):
+    if isinstance(method, PrefixWrapper):
+        return get_settings(method.wrapped)
+    if not issubclass(method, PasswordHash):
+        raise ValueError("method is not a password hash implementation")
+    return set(getattr(method, 'setting_kwds', None) or ())
+
+
 def make_hash(method, password, **params):
     """ Hash a password using a given implementation.
 
@@ -99,6 +120,61 @@ def make_hash(method, password, **params):
         Return the hash cryptstring.
     """
     return getattr(method, PASSLIB_HASH_NAME)(password, **params)
+
+
+_parameters = dict()
+
+
+def param(name):
+    def register(func):
+        _parameters[name] = func
+        return func
+    return register
+
+
+def parse_parameter(parameter, value):
+    if parameter not in _parameters:
+        return parameter
+    return _parameters[parameter](value)
+
+
+@param('block_size')
+@param('digest_size')
+@param('hash_len')
+@param('implicit_rounds')
+@param('memory_cost')
+@param('parallelism')
+@param('rounds')
+@param('salt_len')
+@param('salt_size')
+@param('time_cost')
+def _posint(input_value):
+    return int(input_value)
+
+
+@param('truncate_error')
+def _bool(input_value):
+    try:
+        # key=1, key=0
+        return bool(int(input_value))
+    except ValueError:
+        pass
+    # key=no, key=false
+    if str(input_value).lower().startswith('n'):
+        return False
+    if str(input_value).lower() == 'false':
+        return False
+    # key='' key=anything_else
+    return bool(input_value)
+
+
+@param('algs')
+@param('salt')
+@param('marker')
+@param('variant')
+@param('ident')
+def _unmodified(input_value):
+    return input_value
 
 
 class MethodWrapper(object):
@@ -128,10 +204,27 @@ class MethodWrapper(object):
         return requires_user(self.method)
 
     @property
+    def settings(self):
+        settings = get_settings(self.method)
+        if self.require_user:
+            # 'user' does not appear in the setting_kwds tuple
+            settings.add('user')
+        return settings
+
+    @property
     def supported(self):
         return is_supported(self.method)
 
     def __call__(self, password, **params):
+        if self.require_user and 'user' not in params:
+            raise TypeError(
+                "{0.name} requires a 'user' parameter".format(self))
+
+        for p in params:
+            if p not in self.settings:
+                raise TypeError(
+                    "{0.name} has no parameter {1}".format(self, p))
+
         return make_hash(self.method, password, **params)
 
 
--- mkcrypt/__main__.py
+++ mkcrypt/__main__.py
@@ -3,10 +3,29 @@
 """ Make crypt strings using `passlib`. """
 from __future__ import absolute_import, print_function
 import argparse
+import logging
 import getpass
 import sys
-from functools import partial
-from mkcrypt import methods, __version__
+from itertools import tee as iter_tee
+from mkcrypt import methods, parse_parameter, __version__
+
+logger = logging.getLogger(__name__)
+
+verbosity_levels = [
+    logging.CRITICAL,
+    logging.ERROR,
+    logging.WARNING,
+    logging.INFO,
+    logging.DEBUG,
+]
+
+
+def set_verbosity(verbosity):
+    try:
+        level = verbosity_levels[verbosity]
+    except IndexError:
+        level = logging.DEBUG
+    logging.getLogger().setLevel(level)
 
 
 def get_password_loop(verify=True, allow_empty=False):
@@ -23,138 +42,175 @@ def get_password_loop(verify=True, allow_empty=False):
             return passwd
 
 
-def output_all():
-    """ List methods (columns: name, supported, class, description) """
-    # column headers
-    name_hdr = 'method'
-    cls_hdr = 'class'
-    impl_hdr = 'supported'
-    desc_hdr = 'description'
+def output_columns(method_iterator):
+    """ Column format methods. """
+    # True/False -> yes, no
+    yesno = {True: 'yes', False: 'no'}
 
-    # columns value map
-    impl_map = {True: 'yes', False: 'no'}
+    # Column defs (TODO: move out of this method?)
+    columns = [
+        ('method', lambda m: m.name),
+        ('supported', lambda m: yesno[m.supported]),
+        ('class', lambda m: m.class_name),
+        # ('settings', lambda m: ','.join(m.settings)),
+        # ('description', lambda m: m.description),
+    ]
 
-    # column widths
-    name_len = max(len(name_hdr), max(len(n) for n in methods))
-    cls_len = max(len(cls_hdr),
-                  max(len(m.class_name) for m in methods.values()))
-    impl_len = max(len(impl_hdr), max(len(v) for v in impl_map.values()))
-    desc_len = max(len(desc_hdr), max(len(m.description)
-                                      for m in methods.values()))
+    # calculate column lengths
+    c_length = [0, ] * len(columns)
+    for idx in range(len(columns)):
+        method_iterator, tmp_iter = iter_tee(method_iterator)
+        c_length[idx] = max(len(columns[idx][0]),
+                            max(len(columns[idx][1](m)) for m in tmp_iter))
 
-    row = partial('{0:{w0}}  {1:{w1}}  {2:{w2}}  {3:{w3}}'.format,
-                  w0=name_len, w1=impl_len, w2=cls_len, w3=desc_len)
+    # output method
+    def row(items):
+        return '  '.join('{0:{padding}}'.format(value, padding=c_length[idx])
+                         for idx, value in enumerate(items))
 
-    print(row(name_hdr, impl_hdr, cls_hdr, desc_hdr))
-    print(row('-' * name_len, '-' * impl_len, '-' * cls_len, '-' * desc_len))
-    for m in methods.values():
-        print(row(m.name, impl_map[m.supported], m.class_name, m.description))
-
-
-PARAM_MAP = {
-    'salt_size': int,
-    'rounds_cost': int,
-    'rounds': int,
-}
+    print(row([c[0] for c in columns]))
+    print(row(['-' * l for l in c_length]))
+    for m in method_iterator:
+        print(row([c[1](m) for c in columns]))
 
 
-def param_type(key_value):
+def param_type(raw_value):
     """ Parse parameter input, e.g. foo=bar. """
-    key, sep, value = key_value.partition('=')
+    param, sep, value = raw_value.partition('=')
     if sep != '=':
         raise argparse.ArgumentTypeError(
-            "invalid format ({0})".format(key_value))
-    if not key:
-        raise argparse.ArgumentTypeError("empty parameter")
+            "invalid format ({0})".format(raw_value))
+    if not param:
+        raise argparse.ArgumentTypeError("empty parameter name")
 
-    # Parse int params:
+    # Parse param values:
     try:
-        if key in PARAM_MAP:
-            value = PARAM_MAP[key](value)
-        elif not value:
-            raise ValueError("empty value ('{0}')".format(value))
+        value = parse_parameter(param, value)
     except ValueError as e:
         raise argparse.ArgumentTypeError(
-            "invalid '{0}' value: {1}".format(key, e))
-    return key, value
+            "{0}, {1}".format(param, e))
+    return param, value
 
 
-def main(args=None):
-    supported_methods = [m.name for m in methods.values() if m.supported]
+def make_parser(supported_methods=None):
+    supported_methods = supported_methods or []
+    method_choices = [m.name for m in supported_methods]
 
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        '-v', '--version',
+        "-v", "--verbose",
+        action="count",
+        help="enable/increase output verbosity")
+
+    alt = parser.add_argument_group('alternate actions',
+                                    'Options that change the default '
+                                    'behaviour of this script. '
+                                    'Each option here will cause the script '
+                                    'to dump some info and exit')
+    alt_actions = alt.add_mutually_exclusive_group()
+    alt_actions.add_argument(
+        '--version',
         action='version',
         version=__version__)
-
-    and_exit = parser.add_mutually_exclusive_group()
-    ls = and_exit.add_argument(
-        '-l', '--list',
+    list_m = alt_actions.add_argument(
+        '--list-methods',
         action='store_true',
         default=False,
         help="list supported methods and exit")
-    and_exit.add_argument(
+    list_p = alt_actions.add_argument(
+        '--list-params',
+        action='store_true',
+        default=False,
+        help="list supported parameters and exit")
+    alt_actions.add_argument(
         '--list-all',
         action='store_true',
         default=False,
         help="list all known methods and exit")
-    and_exit.add_argument(
-        '--doc',
-        choices=supported_methods,
+    alt_actions.add_argument(
+        '--show-params',
+        choices=method_choices,
+        metavar='METHOD',
+        default=None,
+        help="show supported parameters for %(metavar)s and exit")
+    alt_actions.add_argument(
+        '--show-docstring',
+        choices=method_choices,
         metavar='METHOD',
         default=None,
         help="show docstring for a given implementation and exit")
 
-    # TODO: type=param_type or nargs=2?
-    parser.add_argument(
-        '-p',
+    main = parser.add_argument_group('make crypt')
+
+    main.add_argument(
+        '-p', '--param',
         dest='params',
         action='append',
         type=param_type,
         metavar=('PARAM=VALUE'),
         default=[],
         help=("set parameters,"
-              " e.g.: `-p ident=2a` or `-p rounds=12`"))
+              " e.g.: `-p ident=2a` or `-p rounds=12`"
+              " (use {0} to see available)".format(list_p.option_strings)))
 
-    parser.add_argument(
+    main.add_argument(
         '-s', '--show-plaintext',
         dest='print_pass',
         action='store_true',
         default=False,
         help="write the plaintext password to stdout")
 
-    parser.add_argument(
+    main.add_argument(
         '--no-verify',
         dest='verify',
         action='store_false',
         default=True,
         help="do not ask to verify password")
 
-    parser.add_argument(
+    main.add_argument(
         'crypt',
-        choices=supported_methods,
+        choices=method_choices,
         metavar='METHOD',
         nargs='?',
         default='scrypt',
         help="hash implementation (use {0} to see"
-             " available)".format('|'.join(ls.option_strings)))
+             " available)".format('|'.join(list_m.option_strings)))
+    return parser
 
-    # TODO: rounds, salt, rounds_cost, salt_size?
 
-    args = parser.parse_args()
+def main(inargs=None):
+    supported_methods = [m for m in methods.values() if m.supported]
+    parser = make_parser(supported_methods=supported_methods)
+    args = parser.parse_args(inargs)
 
-    if args.list:
-        for name in supported_methods:
-            print(name)
+    if args.verbose:
+        set_verbosity(args.verbose)
+
+    logger.debug('args: {0}'.format(repr(args)))
+
+    if args.list_methods:
+        for m in supported_methods:
+            print(m.name)
+        raise SystemExit()
+
+    if args.list_params:
+        params = {p for m in supported_methods for p in m.settings}
+        for param in sorted(params):
+            print(param)
         raise SystemExit()
 
     if args.list_all:
-        output_all()
+        output_columns(iter(methods.values()))
         raise SystemExit()
 
-    if args.doc:
-        m = methods[args.doc]
+    if args.show_params:
+        m = methods[args.show_params]
+        for param in sorted(m.settings):
+            print(param)
+        raise SystemExit()
+
+    if args.show_docstring:
+        m = methods[args.show_docstring]
         help(m.method)
         raise SystemExit()
 
@@ -182,4 +238,7 @@ def main(args=None):
 
 
 if __name__ == '__main__':
+    logging.basicConfig(format="%(levelname)s - %(name)s - %(message)s",
+                        level=verbosity_levels[0],
+                        stream=sys.stderr)
     main()
